57dd40b8b542682c3e46acd15eb58bdcd70be209
vincent <vincent.garry@hotmail.com>
2024-07-22 13:26:28 +0200

    Ajout connexion facebook

---
+++ src/Controller/FacebookController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FacebookController extends AbstractController
+{
+    #[Route('/connect/facebook', name: 'connect_facebook_start')]
+    public function connectAction(ClientRegistry $clientRegistry): RedirectResponse
+    {
+        return $clientRegistry
+            ->getClient('facebook')
+            ->redirect([
+                'public_profile', 'email', 'user_birthday', 'user_gender'
+            ]);
+    }
+
+    #[Route('/connect/facebook/check', name: 'connect_facebook_check')]
+    public function connectCheckAction()
+    {
+        // Cette méthode ne sera jamais exécutée,
+        // car le flux sera intercepté par le FacebookAuthenticator
+    }
+}
