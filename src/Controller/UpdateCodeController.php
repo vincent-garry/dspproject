8e067b32efc5d253f0b4ce29445c3625bc4f4d23
vincent <vincent.garry@hotmail.com>
2024-07-23 13:21:29 +0200

    modification api

--- src/Controller/UpdateCodeController.php
+++ src/Controller/UpdateCodeController.php
@@ -33,7 +33,7 @@ class UpdateCodeController extends AbstractController
         }
 
         // Rechercher l'utilisateur par son adresse e-mail
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $userEmail]);
+        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => 'vgarry7@gmail.com']);
 
         if (!$user) {
             throw new NotFoundHttpException('User not found');
