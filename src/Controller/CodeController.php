56a8e0c4d79749ed2e0d536cab7ec4e050e91bb4
vincent <vincent.garry@hotmail.com>
2024-07-23 13:47:35 +0200

    modification api

---
+++ src/Controller/CodeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Code;
+use App\Entity\User;
+use App\Repository\CodeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CodeController extends AbstractController
+{
+    private CodeRepository $codeRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(CodeRepository $codeRepository, EntityManagerInterface $entityManager)
+    {
+        $this->codeRepository = $codeRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/api/codes/{code}', name: 'get_code', methods: ['GET'])]
+    public function getCode(string $code): JsonResponse
+    {
+        $codeEntity = $this->codeRepository->findOneBy(['code' => $code]);
+
+        if (!$codeEntity) {
+            throw new NotFoundHttpException('Code not found');
+        }
+
+        return new JsonResponse([
+            'code' => $codeEntity->getCode(),
+            'prize' => $codeEntity->getPrize(),
+            'isUsed' => $codeEntity->isUsed(),
+        ]);
+    }
+
+    #[Route('/api/codes/{code}/associate_user', name: 'associate_code_to_user', methods: ['PATCH'])]
+    public function associateUser(string $code, Request $request): JsonResponse
+    {
+        $codeEntity = $this->codeRepository->findOneBy(['code' => $code]);
+
+        if (!$codeEntity) {
+            throw new NotFoundHttpException('Code not found');
+        }
+
+        $data = json_decode($request->getContent(), true);
+        $userEmail = $data['userEmail'] ?? null;
+
+        if (!$userEmail) {
+            throw new BadRequestHttpException('User email is required');
+        }
+
+        // Assume User is another entity and we can find it by email
+        $user = $this->entityManager->getRepository(User::class)->findOneBy(['email' => $userEmail]);
+
+        if (!$user) {
+            throw new NotFoundHttpException('User not found');
+        }
+
+        $codeEntity->setUsers($user);
+        $codeEntity->setIsUsed(true);
+        $this->entityManager->flush();
+
+        return new JsonResponse([
+            'code' => $codeEntity->getCode(),
+            'prize' => $codeEntity->getPrize(),
+            'isUsed' => $codeEntity->isUsed(),
+            'associatedUser' => $user->getEmail(),
+        ]);
+    }
+}
---
+++ src/Controller/UserController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    private UserRepository $userRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager)
+    {
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/api/users', name: 'get_users', methods: ['GET'])]
+    public function getUsers(): JsonResponse
+    {
+        $users = $this->userRepository->findAll();
+
+        $userData = [];
+        foreach ($users as $user) {
+            $userData[] = [
+                'email' => $user->getEmail(),
+                'codes' => array_map(function ($code) {
+                    return [
+                        'code' => $code->getCode(),
+                        'prize' => $code->getPrize(),
+                        'isUsed' => $code->isUsed(),
+                    ];
+                }, $user->getCodes()->toArray())
+            ];
+        }
+
+        return new JsonResponse($userData);
+    }
+
+    #[Route('/api/users/{email}', name: 'get_user', methods: ['GET'])]
+    public function getUser(string $email): JsonResponse
+    {
+        $user = $this->userRepository->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            throw new NotFoundHttpException('User not found');
+        }
+
+        return new JsonResponse([
+            'email' => $user->getEmail(),
+            'codes' => array_map(function ($code) {
+                return [
+                    'code' => $code->getCode(),
+                    'prize' => $code->getPrize(),
+                    'isUsed' => $code->isUsed(),
+                ];
+            }, $user->getCodes()->toArray())
+        ]);
+    }
+}
+
