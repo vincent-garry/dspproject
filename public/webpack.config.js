d41dc012139ea1f03d91ddd9148063670dcc8548
Patrice-Emile <76409884+Patrice-Emile@users.noreply.github.com>
2024-07-24 16:14:33 +0200

    home css

--- public/webpack.config.js
+++ public/webpack.config.js
@@ -1,31 +1,32 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require("@symfony/webpack-encore");
 
 if (!Encore.isRuntimeEnvironmentConfigured()) {
-    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || "dev");
 }
 
-Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './assets/app.js')
-    .enableStimulusBridge('./assets/controllers.json')
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
-    .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
-    .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
-    })
-    .enableSassLoader()
-    .configureCssLoader(options => { options.modules = true})
-    .configureDevServerOptions(options => {
-        options.devMiddleware = {
-            writeToDisk: true,
-        }
-    })
-;
+Encore.setOutputPath("public/build/")
+  .setPublicPath("/build")
+  .addEntry("app", "./assets/app.js")
+  .addStyleEntry("styles", "./style/style.scss")
+  .enableStimulusBridge("./assets/controllers.json")
+  .splitEntryChunks()
+  .enableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = "usage";
+    config.corejs = "3.23";
+  })
+  .enableSassLoader()
+  .configureCssLoader((options) => {
+    options.modules = true;
+  })
+  .configureDevServerOptions((options) => {
+    options.devMiddleware = {
+      writeToDisk: true,
+    };
+  });
 
 module.exports = Encore.getWebpackConfig();
